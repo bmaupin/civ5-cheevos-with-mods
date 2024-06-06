c5ccd036da240004f3b0fdbe208347bdc1139f11
bmaupin <bmaupin@users.noreply.github.com>
2024-06-06 12:42:35 -0400

    build: Replace pkg with nexe
    nexe makes a smaller binary out of the box, and this allows using Node 10 which further reduces the size of the binary

--- archive/scripts/build.sh
+++ archive/scripts/build.sh
@@ -4,5 +4,5 @@ npm install
 # Bundle; this is necesssary for ESM support (https://github.com/vercel/pkg/issues/1291#issuecomment-1295792641)
 node_modules/.bin/esbuild src/civPatcher.ts --bundle --platform=node --target=node12 --outfile=src/civPatcher.js
 # Build the package
-node_modules/.bin/pkg src/civPatcher.js -o patch-civ5 -t node12-win
+node_modules/.bin/nexe src/civPatcher.js -o patch-civ5 -t windows-x86-10.16.3
 7z a patch-civ5.7z patch-civ5.exe
