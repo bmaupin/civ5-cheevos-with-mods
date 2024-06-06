e925f5839375e949f6723beeca4ce7294d3aec79
bmaupin <bmaupin@users.noreply.github.com>
2024-06-06 13:03:41 -0400

    build: Compress Node executable with upx
    This reduces the size of the executable from ~22 MB to 6 MB, eliminating the need for 7zip 😎

--- archive/scripts/build.sh
+++ archive/scripts/build.sh
@@ -3,6 +3,11 @@
 npm install
 # Bundle; this is necesssary for ESM support (https://github.com/vercel/pkg/issues/1291#issuecomment-1295792641)
 node_modules/.bin/esbuild src/civPatcher.ts --bundle --platform=node --target=node12 --outfile=src/civPatcher.js
-# Build the package
+if [[ ! -f ~/.nexe/windows-x86-10.16.3 ]]; then
+    # Build the package once to fetch Node
+    node_modules/.bin/nexe src/civPatcher.js -o patch-civ5 -t windows-x86-10.16.3
+    # Compress the Node executable
+    upx --lzma ~/.nexe/windows-x86-10.16.3
+fi
+# Build the package with the compressed version of Node
 node_modules/.bin/nexe src/civPatcher.js -o patch-civ5 -t windows-x86-10.16.3
-7z a patch-civ5.7z patch-civ5.exe
